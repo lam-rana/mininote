5f69bb1e67ed1048392f79c6bcb574f86ae75452
lam-rana <lamarana.mld83@gmail.com>
2024-01-25 01:07:08 +0100

    frist commit

---
+++ Content/content_signup.php
@@ -0,0 +1,15 @@
+<div class="row">
+        <div class="col-md-6 offset-md-3 Signup">
+            <h2>Inscription </h2>
+            <form class='form-outline'>
+                    <input class='form-control mr-sm-2' type='text' placeholder='Name' name='Name'>
+                    <input class='form-control mr-sm-2' type='text' placeholder='Login' name='login'>
+                    <input class='form-control mr-sm-2' type='password' placeholder='Mot de passe' name='psw'>
+                    <input class='form-control mr-sm-2' type='password' placeholder='Confirmation Mot de passe' name='psw'>
+                    <div class="d-flex justify-content-center align-items-center">
+  <button class="btn btn-inline-light" type="submit" style="color: black;">
+    Signup
+  </button>
+</div>
+                </form>
+</div>
